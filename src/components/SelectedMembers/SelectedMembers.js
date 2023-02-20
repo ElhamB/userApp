9125d0e97e1191f0b1a513637e3396de6beaa874
Elham <it.ebagheri@gmail.com>
2023-02-20 16:02:50 +0330

    selected user

--- src/components/SelectedMembers/SelectedMembers.js
+++ src/components/SelectedMembers/SelectedMembers.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useSelector } from "react-redux";
 import "./selectedMember.css";
 import SelectedMemberItem from "./SelectedMemberItem";
 
 const SelectedMembers = () => {
   const selectedUsers = useSelector((state) => state.user.selectedUsers);
-
+  const count = selectedUsers.length - 6;
   return (
-    <div className="selected-container">
-      {
-        selectedUsers.map((user)=><SelectedMemberItem user={user} key={user.id}/>)
-      }
-      <div className="selected-counter">+5</div>
-    </div>
+    <Fragment>
+      {selectedUsers.length === 0 && (
+        <div className="no-item">Selected members will display here</div>
+      )}
+      <div className="selected-container">
+        {selectedUsers.slice(0,6).map((user) => (
+          <SelectedMemberItem user={user} key={user.id} />
+        ))}
+        {selectedUsers.length > 6 && (
+          <div className="selected-counter">+{count}</div>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
